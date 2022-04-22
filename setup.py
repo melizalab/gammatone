5cee9aa75ac4d53259be5105ddde08b2fd5046a5
Jonah Weissman <jonahrweissman@gmail.com>
2022-04-22 10:05:47 -0400

    Release 0.1.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "Gammatone",
-    version = "1.0",
+    version = "0.1.0",
     packages = find_packages(),
 
     install_requires = [
